a641d0a83ad0210db6d6b45223433946899d0808
emretnkl <emretnkl94@gmail.com>
2022-10-07 21:52:59 +0300

    Assignment 3 has been completed.
    Assignment 3 has been completed.

---
+++ app/src/main/java/com/emretnkl/rvadapterexample/adapter/BooksAdapter.kt
@@ -0,0 +1,11 @@
+package com.emretnkl.rvadapterexample.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class BooksAdapter() : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
